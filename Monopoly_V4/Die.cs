396a0cf5d21eb0e7de942d7f80db0b06b99b7b4c
iwamcoding <iwam31023@gmail.com>
2025-04-06 05:40:36 +0300

    Add project files.

---
+++ Monopoly_V4/Die.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monopoly_V4
+{
+    public class Die
+    {
+        public int? Number { get; set; }
+        private int minValue;
+        private int maxValue;
+        private Random rnd;
+
+        public Die() : this(1, 6)
+        {
+            Number = null;
+        }
+
+        public Die(int minValue, int maxValue)
+        {
+            this.minValue = minValue;
+            this.maxValue = maxValue;
+            this.rnd = new Random();
+        }
+
+
+        public void Roll()
+        {
+            Number = rnd.Next(minValue, maxValue);
+        }
+
+
+        public void Clear()
+        {
+            Number = null;
+        }
+    }
+}
